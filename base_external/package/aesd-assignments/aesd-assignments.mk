4eabede2a5510a15f6a411ea476e7a66627ccafb
Sona Subair <sosu5785@colorado.edu>
2022-09-19 20:20:07 -0600

    Modifications

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -5,7 +5,7 @@
 #
 ##############################################################
 
-AESD_ASSIGNMENTS_VERSION = 'e45dc15100e27cb668edf0d789b9ad39ac1045b7'
+AESD_ASSIGNMENTS_VERSION = '435af21bb89bef2d1e4314288a64f89da53c5560'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
