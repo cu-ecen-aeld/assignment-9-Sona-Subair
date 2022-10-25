588e9bcef8abbb7fdcc1c9e7692e7231f30f2aa6
Sona Subair <sosu5785@colorado.edu>
2022-10-25 01:20:37 -0600

    Changes

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -5,7 +5,7 @@
 #
 ##############################################################
 
-AESD_ASSIGNMENTS_VERSION = '54c041181f6563240fb9c9f9390fc6cb25bbf0b7'
+AESD_ASSIGNMENTS_VERSION = 'ac0c1e0608bbe46d6c3ff81b39c51d29fb62b812'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -15,6 +15,7 @@ AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server all	
 endef
 
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
@@ -22,6 +23,8 @@ define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
 	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin
 	$(INSTALL) -m 0755 $(@D)/finder-app/finder.sh $(TARGET_DIR)/bin
+	$(INSTALL) -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/server/aesdsocketstart-stop-daemon.sh $(TARGET_DIR)/etc/init.d	
 	$(INSTALL) -m 0755 $(@D)/finder-app/writer $(TARGET_DIR)/bin	
 	$(INSTALL) -m 0755 $(@D)/finder-app/finder-test.sh $(TARGET_DIR)/bin		
 endef
