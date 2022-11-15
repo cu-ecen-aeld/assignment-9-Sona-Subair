fc5c26bbb50e7833261705676ea0d269173b7946
Sona Subair <sosu5785@colorado.edu>
2022-11-15 01:02:39 -0700

    Full-test passed

--- base_external/package/aesd-char-driver/aesd-char-driver.mk
+++ base_external/package/aesd-char-driver/aesd-char-driver.mk
@@ -4,7 +4,7 @@
 #
 ##############################################################
 
-AESD_CHAR_DRIVER_VERSION = '48d98ec7b66b635dd7e25baa067a52443d879485'
+AESD_CHAR_DRIVER_VERSION = '76040d8830fbdad331a4d081d9407d29c7cc980c'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
